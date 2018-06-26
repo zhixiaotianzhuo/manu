a31184a7ff9880dee41f04855ef6f279b6518bad
zhixiaotianzhuo <zhixiaotianzhuo@163.com>
2018-06-26 09:04:01 +0800

    项目初始化 - 多环境配置 + 日志配置 + Controller 实现

--- src/main/java/com/hetong/ad/mi/manu/ManuApplication.java
+++ src/main/java/com/hetong/ad/mi/manu/ManuApplication.java
@@ -3,9 +3,22 @@ package com.hetong.ad.mi.manu;
 import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ManuApplication {
+public class ManuApplication extends SpringBootServletInitializer {
+
+	/**
+	 * 打包成部署的 war 包
+	 * @param application
+	 * @return
+	 */
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		/*SpringbootApplication 类继承 SpringBootServletInitializer 并重写 configure 方法*/
+		return application.sources(ManuApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication app = new SpringApplication(ManuApplication.class);
